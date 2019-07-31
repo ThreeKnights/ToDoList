4a34b25cfabe5617ba469f34c8625503202ed2dd
abhishekpakhare97 <abhishekpakhare1997@gmail.com>
2019-07-31 21:14:50 +0530

    adding javascript

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.getElementById("BtnCustom").addEventListener("click", function() {
+  let tasktaker = document.getElementById("tasktaker").value;
+  document.getElementById("CheckBoxLabel").text = tasktaker;
+
+  //alert(tasktaker);
+});
+
+
